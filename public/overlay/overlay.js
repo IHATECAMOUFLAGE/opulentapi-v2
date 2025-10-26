56a4a9e988feaaa6e5caf03fbeaaf4d573dccc2b
IHATECAMOUFLAGE <ihatecamouflage@hotmail.com>
2025-10-26 10:11:37 -0400

    Create overlay.js

---
+++ public/overlay/overlay.js
@@ -0,0 +1,6 @@
+(function() {
+  const badge = document.createElement('div');
+  badge.className = 'proxy-badge';
+  badge.innerText = 'Proxy Mode';
+  document.documentElement.appendChild(badge);
+})();
